7bbef78782e6699497bc93c4ee7dcb7055b43722
ChristianSheherlis <57498743+ChristianSheherlis@users.noreply.github.com>
2021-08-03 10:54:11 -0400

    effects

--- src/index.js
+++ src/index.js
@@ -7,7 +7,13 @@ gsap.registerEffect({
     return gsap.to(targets, { duration: config.duration, opacity: 0 });
   },
   defaults: { duration: 2 }, // defaults can be used as in "config" objects for reuseability
-  extendTimeline: true, //extendTimeline allows you to call an effect directly on a timeline
+  extendTimeline: true, // extendTimeline allows you to call an effect directly on a timeline
 });
 
-gsap.effects.fade(".box"); //when calling an effect you can call
+gsap.effects.fade(".box"); // you can call an effect by writing gsap.effects.name(class)
+
+let tl = gsap.timeline();
+
+tl.effects.fade(".box4");
+tl.effects.fade(".box5");
+tl.effects.fade(".box6");
